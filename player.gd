9b37c17c83f261a2293b183ef4e8d068ae92db5f
= <halfdanemb@gmail.com>
2023-10-09 19:44:00 +0200

    Improved the level (read description)
    Changes:
    - Extra auto-tile-set, called wood created
    - Player now has and uses the idle, run, jump and fall animations
    - Player movement constants tweaked to better suit level size
    - "die" method implemented on player that simply reloads the current
      scene
    - World background implemented
    - Deathbox implemented, meaning that player no longer falls infinitely
      (die method triggered)
    - Platform placement tweaked, and more collectables placed
    
    Fixes:
    - Missing textures fixed (seems that file path had changed, so textures
      had to be replaced, and auto-tile masks needed to be repainted

--- player.gd
+++ player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -350.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,17 +12,32 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		#Play jump animation if rising, and fall animation if falling
+		if sign(velocity.y) == -1:
+			$AnimatedSprite2D.play("jump")
+		else:
+			$AnimatedSprite2D.play("fall")
+	
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		
+		if is_on_floor():
+			$AnimatedSprite2D.flip_h = sign(direction) == -1
+			$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
+
+
+func die():
+	get_tree().reload_current_scene()
